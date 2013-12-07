fa1ec71136c6646e0f41536400cae88b0c133af8
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2013-12-07 15:32:58 +0800

    Improved tests for "core.backends".

--- djangoerp/core/tests/test_backends.py
+++ djangoerp/core/tests/test_backends.py
@@ -23,6 +23,13 @@ from ..models import *
 from ..backends import *
 
 class ObjectPermissionBackendTestCase(TestCase):
+    def test_always_fail_authentication(self):
+        """Tests the (unsupported) user authentication must always fail.
+        """
+        u, n = get_user_model().objects.get_or_create(username="u", password="password")
+        
+        self.assertEqual(ob.authenticate("u1", "password"), None)
+        
     def test_has_perm(self):
         """Tests simple object permissions between three different instances.
         """
@@ -47,6 +54,36 @@ class ObjectPermissionBackendTestCase(TestCase):
         
         self.assertTrue(ob.has_perm(u2, p, u))
         self.assertFalse(ob.has_perm(u2, p, u1))
+        
+    def test_superuser_has_always_perm(self):
+        """Tests that a superuser must have global perms on everything.
+        """
+        class TestObject(object):
+            pk = 2894328923984238943298
+            
+        obj = TestObject()
+        su, n = get_user_model().objects.get_or_create(username="superuser", is_superuser=True)
+        
+        p = Permission.objects.get_by_natural_key("delete_user", auth_app, "user")
+        
+        self.assertTrue(ob.has_perm(su, p, obj))
+        
+    def test_anonymous_has_never_perm(self):
+        """Tests that an anonymous user doesn't have any perms on anything.
+        """
+        class TestObject(object):
+            pk = 5894325454554428943296
+            
+        def fake_is_anonymous():
+            return True
+            
+        obj = TestObject()
+        au, n = get_user_model().objects.get_or_create(username="anonymous")
+        au.is_anonymous = fake_is_anonymous
+        
+        p = Permission.objects.get_by_natural_key("delete_user", auth_app, "user")
+        
+        self.assertFalse(ob.has_perm(au, p, obj))
         
     def test_has_perm_by_name(self):
         """Tests retrieving object permissions by names.
