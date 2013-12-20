695e07e06aa79493d3f1cb0063f61306d2f14e3f
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2013-12-20 14:13:47 +0800

    Added tests for "menus.views.BookmarkMixin".

--- djangoerp/menus/tests/test_views.py
+++ djangoerp/menus/tests/test_views.py
@@ -21,6 +21,18 @@ from django.contrib.auth import get_user_model
 from . import *
 from ..views import *
 from ..views import _get_bookmarks, _get_bookmark # NOTE: not in public API!
+        
+class _FakeBaseView(object):
+    def __init__(self, *args, **kwargs):
+        self.request = FakeRequest()
+        self.args = args
+        self.kwargs = kwargs
+        
+    def get_initial(self):
+        return {}
+        
+    def get_queryset(self):
+        return Bookmark.objects.all()    
 
 class GetterFunctionsTestCase(TestCase):
     def setUp(self):
@@ -39,28 +51,47 @@ class GetterFunctionsTestCase(TestCase):
         """Tests _get_bookmark getter function.
         """
         self.assertEqual(_get_bookmark(self.request, slug="bookmark"), self.user_bookmark)
-    
-class BookmarkCreateUpdateMixinTestCase(TestCase):
-    def setUp(self):
-        class _FakeBaseView(object):
-            def get_initial(self):
-                return {}
-                
-        class FakeBookmarkCreateUpdateView(BookmarkCreateUpdateMixin, _FakeBaseView):
+        
+class BookmarkMixinTestCase(TestCase):
+    def setUp(self):                
+        class TestBookmarkMixin(BookmarkMixin, _FakeBaseView):
             pass
             
-        self.v = FakeBookmarkCreateUpdateView()
+        self.m = TestBookmarkMixin()
+        self.u, n = get_user_model().objects.get_or_create(username="u")
+        self.m.request.user = self.u
+        self.user_bookmarks = Menu.objects.get(slug="user_1_bookmarks")
+        self.user_bookmark1 = Bookmark.objects.create(title="Bookmark 1", slug="bookmark1", url="/", menu=self.user_bookmarks)
+        self.user_bookmark2 = Bookmark.objects.create(title="Bookmark 2", slug="bookmark2", url="/", menu=self.user_bookmarks)
+        
+    def test_get_queryset(self):
+        """Tests "BookmarkMixin.get_queryset" method.
+        """
+        self.assertEqual(self.m.get_queryset().count(), 2)
+        self.assertQuerysetEqual(
+            self.m.get_queryset(),
+            [
+                repr(self.user_bookmark1),
+                repr(self.user_bookmark2)
+            ],
+            ordered=False
+        )          
+    
+class BookmarkCreateUpdateMixinTestCase(TestCase):
+    def setUp(self):                
+        class TestBookmarkCreateUpdateView(BookmarkCreateUpdateMixin, _FakeBaseView):
+            pass
+            
+        self.v = TestBookmarkCreateUpdateView()
         
     def test_get_initial_url_on_creation(self):
         """Tests the initial URL is set on the current path on bookmark creation.
         """
-        self.v.request = FakeRequest()
         self.v.object = None
         self.assertEqual(self.v.get_initial(), {"url": "/bookmarks/"})
         
     def test_get_initial_url_on_editing(self):
         """Tests the initial URL is not set on bookmark editing.
         """
-        self.v.request = FakeRequest()
         self.v.object = Bookmark()
         self.assertEqual(self.v.get_initial(), {})
