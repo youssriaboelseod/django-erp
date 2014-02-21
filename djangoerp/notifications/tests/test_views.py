a3543bfb0d15c432bced5584e0964392300c3cb6
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2014-02-22 02:28:02 +0800

    Added tests for "notifications.views.ListNotificationView".

--- djangoerp/notifications/tests/test_views.py
+++ djangoerp/notifications/tests/test_views.py
@@ -19,7 +19,7 @@ from django.test import TestCase
 from django.test.client import RequestFactory
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
-from djangoerp.core.models import ObjectPermission
+from djangoerp.core.models import Permission, ObjectPermission
 
 from ..models import *
 from ..views import *
@@ -101,6 +101,8 @@ class ObjectFollowViewTestCase(TestCase):
         view_kwargs = {"object_model": "user", "object_id": self.obj.pk}
         
         request = self.factory.get(reverse("object_follow", kwargs=view_kwargs))
+        
+        # With invalid follower.
         request.user = self.u3
         
         response = object_follow(request, **view_kwargs)
@@ -109,6 +111,7 @@ class ObjectFollowViewTestCase(TestCase):
         self.assertFalse(self.obj.is_followed_by(self.u3))
         self.assertEqual(response.url, reverse("user_login")[:-1] + "?next=%s" % request.get_full_path())
         
+        # With valid follower.
         request.user = self.u2
         request.META["HTTP_REFERER"] = "/success-redirect"
         
@@ -140,6 +143,8 @@ class ObjectUnfollowViewTestCase(TestCase):
         view_kwargs = {"object_model": "user", "object_id": self.obj.pk}
         
         request = self.factory.get(reverse("object_unfollow", kwargs=view_kwargs))
+        
+        # With invalid follower.
         request.user = self.u3
         
         self.assertFalse(self.obj.is_followed_by(self.u3))
@@ -150,6 +155,7 @@ class ObjectUnfollowViewTestCase(TestCase):
         self.assertFalse(self.obj.is_followed_by(self.u3))
         self.assertEqual(response.url, reverse("user_login")[:-1] + "?next=%s" % request.get_full_path())
         
+        # With valid follower.
         request.user = self.u2
         request.META["HTTP_REFERER"] = "/success-redirect"
         
@@ -159,3 +165,43 @@ class ObjectUnfollowViewTestCase(TestCase):
         
         self.assertFalse(self.obj.is_followed_by(self.u2))
         self.assertEqual(response.url, "/success-redirect")
+        
+class ListNotificationViewTestCase(TestCase):
+    def test_permissions(self):
+        """Tests view's permissions.
+        """        
+        user_model = get_user_model()
+        
+        obj = user_model.objects.create(username="u1")
+        
+        u2 = user_model.objects.create_user(username="u2", email="u2@u.it", password="p")
+        u3 = user_model.objects.create_user(username="u3", email="u3@u.it", password="p")
+        
+        vp, n = Permission.objects.get_or_create_by_uid("notifications.view_notification")
+        op, n = ObjectPermission.objects.get_or_create_by_uid("core.view_user.%d" % obj.pk)
+        op.users.add(u2)
+        u2.user_permissions.add(vp)
+        
+        obj.add_followers(u2)
+        
+        url = reverse('notification_list', kwargs={"object_model": "user", "object_id": obj.pk})
+        
+        # With invalid follower.
+        self.assertFalse(obj.is_followed_by(u3))
+        
+        self.client.login(username="u3", password="p")
+        
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(obj.is_followed_by(u3))
+        self.assertEqual(response.url, "http://testserver" + reverse("user_login")[:-1] + "?next=%s" % url)
+        
+        # With valid follower.
+        self.assertTrue(obj.is_followed_by(u2))
+        
+        self.client.login(username="u2", password="p")
+        
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, 200)
